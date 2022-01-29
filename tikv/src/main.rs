c1812353239dbc98cef34857833bccc8674ca319
Ao Song <ao.song@outlook.com>
2022-01-29 22:01:36 +0100

    fix the example

--- tikv/src/main.rs
+++ tikv/src/main.rs
@@ -1,7 +1,7 @@
 use tikv_client::{TransactionClient, Error};
 
 async fn run() -> Result<(), Error> {
-    let txn_client = TransactionClient::new(vec!["127.0.0.1:2379"], None).await?;
+    let txn_client = TransactionClient::new(vec!["127.0.0.1:2379"]).await?;
     let mut txn = txn_client.begin_optimistic().await?;
     txn.put("key".to_owned(), "value".to_owned()).await?;
     let value = txn.get("key".to_owned()).await?;
